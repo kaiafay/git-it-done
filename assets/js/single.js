ff4e7dcafee1d686e7bc8e87455bff1aeaa3797a
Kaia Scheirman <kfscheirman18@gmail.com>
2022-02-25 16:35:58 -0800

    fix link to github issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,3 +1,4 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
@@ -60,11 +61,11 @@ var displayIssues = function(issues) {
 
 var displayWarning = function(repo) {
     // add text to warning container
-    limitWarningEl.textContent = "To see more than 30 issues, ";
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
 
     // create a link element to redirect users to Github issues
     var linkEl = document.createElement("a");
-    linkEl.textContent = "See More Issues on Github.com";
+    linkEl.textContent = "Github.com";
     linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
     linkEl.setAttribute("target", "_blank");
 
